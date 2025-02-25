534d5a0ac86fe8f2d83ca3b1337742e634a216ac
Jonathan Wassermann <jonathan.wassermann01@gmail.com>
2025-02-25 18:01:43 +0100

    update_exchange function

--- src/main.py
+++ src/main.py
@@ -2,23 +2,27 @@ from datetime import datetime
 from modules.reader import CurrencyReader
 from modules.visualizer import Visualizer
 
+CHECKINTERVALL = 24 * 60 * 60
 OUTPUT_FOLDER = "data/"
+CURRENCIES: list[str] = [
+    "Australian Dollar",
+    "US Dollar",
+    "Israeli Shekel",
+    "Cuban Peso",
+    "Japanese Yen",
+]
 
-if __name__ == "__main__":
-    currencies: list[str] = [
-        "Australian Dollar",
-        "US Dollar",
-        "Israeli Shekel",
-        "Cuban Peso",
-        "Japanese Yen",
-    ]
-    r = CurrencyReader("Euro", currencies)
+
+def update_exchange() -> None:
+    r = CurrencyReader("Euro", CURRENCIES)
     exchange_df = r.read_timeinterval(
-        start_date=datetime(2024, 4, 1),
-        end_date=datetime(2024, 7, 1))
+        start_date=datetime(2024, 4, 1), end_date=datetime(2024, 7, 1)
+    )
     print(exchange_df)
     exchange_df.to_parquet(OUTPUT_FOLDER + "output.parquet")
 
+
+if __name__ == "__main__":
     v = Visualizer(OUTPUT_FOLDER + "output.parquet")
     v.read_fom_parquet()
     fig = v.create_scatter_plot()
--- src/modules/writer.py
+++
@@ -1,4 +0,0 @@
-
-
-def write_to_parquet():
-    pass
